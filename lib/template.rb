4e2d14366b5b2759283f9d0b0c55271eff2db0d2
Todd Pickell <toddpickell@MacMini.local>
2013-05-28 16:24:42 -0500

    finished template pattern

--- lib/template.rb
+++ lib/template.rb
@@ -1,3 +1,25 @@
 class BaseMonitor
+  attr_reader :inputs
+  def initialize
+    @inputs = :hdmi, :dvi, :vga
+  end
 
+  def size
+    raise AbstractError, "Called abstract method"
+  end
+end
+
+class BigMonitor < BaseMonitor
+  def size
+    27
+  end
+end
+
+class LittleMonitor < BaseMonitor
+  def size 
+    17
+  end
+end
+
+class AbstractError < StandardError
 end
--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -5,8 +5,53 @@ describe BaseMonitor do
     @base = BaseMonitor.new
   end
 
-  it "should have inputs on initialization" do
-    @base.inputs.should = [:hdmi, :dvi, :vga]
+  describe "newly initialized monitor" do
+    it "should have inputs on initialization" do
+      @base.inputs.should == [:hdmi, :dvi, :vga]
+    end
   end
-  
+
+  describe "#size" do
+    it "should throw called abstract method" do
+      expect {@base.size}.to raise_exception(AbstractError, "Called abstract method")
+    end
+  end
+end
+
+
+
+describe BigMonitor do
+  before(:each) do
+    @big = BigMonitor.new
+  end
+
+  describe "newly initialized big monitor" do
+    it "should have inputs on initialization" do
+      @big.inputs.should == [:hdmi, :dvi, :vga]
+    end
+  end
+
+  describe "#size" do
+    it "should have a size of 27 inches" do
+      @big.size.should == 27
+    end
+  end
+end
+
+describe LittleMonitor do
+  before(:each) do
+    @little = LittleMonitor.new
+  end
+
+  describe "newly initialized little monitor" do
+    it "should have inputs on initialization" do
+      @little.inputs.should == [:hdmi, :dvi, :vga]
+    end
+  end
+
+  describe "#size" do
+    it "should have a size of 17 inches" do
+      @little.size.should == 17
+    end
+  end
 end
